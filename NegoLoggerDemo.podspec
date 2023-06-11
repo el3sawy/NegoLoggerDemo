b372f43079e08a0fc59825b0a61db08932cfbf67
el3sawy <ahmedmaelesawy@gmail.com>
2023-06-11 22:25:04 +0300

    add home view

--- NegoLoggerDemo.podspec
+++ NegoLoggerDemo.podspec
@@ -89,7 +89,7 @@ Pod::Spec.new do |spec|
   #  Not including the public_header_files will make all headers public.
   #
 
-  spec.source_files  = "NegoLoggerDemo/**/*.{swift}"
+  spec.source_files  = "NegoLoggerDemo/**/*.{swift,.xib}"
   spec.swift_versions = "5.0"
 
   # spec.public_header_files = "Classes/**/*.h"
---
+++ NegoLoggerDemo/NegoLoggerDemo/Views/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  NegoLoggerDemo
+//
+//  Created by NTG on 11/06/2023.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
