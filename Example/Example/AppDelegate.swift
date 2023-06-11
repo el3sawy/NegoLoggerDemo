40b93acdc7c1026cedcaff02eb3a14ca36c06d31
el3sawy <ahmedmaelesawy@gmail.com>
2023-06-11 22:22:47 +0300

    add presenter

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 //        NegoLogger.log(event: <#T##String#>)
+        BaseViewModel().fetchData()
         return true
     }
 
---
+++ Example/Pods/NegoLoggerDemo/NegoLoggerDemo/NegoLoggerDemo/BaseViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewModel.swift
+//  NegoLoggerDemo
+//
+//  Created by NTG on 11/06/2023.
+//
+
+import Foundation
+
+public class BaseViewModel {
+    
+    public func fetchData() {
+        print("Hoooooo")
+    }
+}
